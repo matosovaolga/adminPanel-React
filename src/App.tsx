4ea63bb0e225fb8ac33f0896296c1fff3b457df9
matosovaolga <olgamatosova@Olgas-Air.N4>
2022-07-29 01:09:32 +0300

    ëdit user: personal details

--- src/App.tsx
+++ src/App.tsx
@@ -10,27 +10,47 @@ import User from './pages/user/User';
 import { Sidebar, TopBar } from './components';
 import EditUser from './pages/editUser/EditUser';
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			light: '#deeeff',
+			main: '#405189',
+			dark: '#303c66',
+			contrastText: '#fff',
+		},
+		secondary: {
+			light: '#fff',
+			main: '#e5faf2',
+			dark: '#0ab39c',
+			contrastText: '#0ab39c',
+		},
+	},
+});
 
 
 export default function App(): JSX.Element {
 	return (
 		<Router>
-			<div className={styles.main}>
-				<Sidebar />
-				<div className={styles.container}>
-					<TopBar messages={9} />
-					<Routes>
-						<Route>
+			<ThemeProvider theme={theme}>
+				<div className={styles.main}>
+					<Sidebar />
+					<div className={styles.container}>
+						<TopBar messages={9} />
+						<Routes>
+							<Route>
 
-							<Route path="/" element={<Home />} />
-							<Route path="/users" element={<UserList />} />
-							<Route path="/user/:userId" element={<User />} />
-							<Route path="/user/:userId/editUser/" element={<EditUser />} />
+								<Route path="/" element={<Home />} />
+								<Route path="/users" element={<UserList />} />
+								<Route path="/user/:userId" element={<User />} />
+								<Route path="/user/:userId/editUser/" element={<EditUser />} />
 
-						</Route>
-					</Routes>
+							</Route>
+						</Routes>
+					</div>
 				</div>
-			</div>
+			</ThemeProvider>
 		</Router>
 
 	);
