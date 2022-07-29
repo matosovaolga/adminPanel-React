74d84973d62f6cfc9f9a486b4b26e2eb7803b3f9
Olga Matosova <matosovaolga@gmail.com>
2022-07-29 22:36:43 +0300

    change pass, upload image component

--- src/pages/editUser/EditUser.tsx
+++ src/pages/editUser/EditUser.tsx
@@ -1,11 +1,15 @@
 import { faker } from "@faker-js/faker";
 import { AddOutlined, GitHub, LanguageOutlined, ModeEditOutlineOutlined, Pinterest, Twitter } from "@mui/icons-material";
-import { Avatar, Box, Button, Chip, OutlinedInput, Slider, Tab, Tabs, TextField } from "@mui/material";
+import { Avatar, Box, Chip, OutlinedInput, Slider, Tab, Tabs } from "@mui/material";
 import React from "react";
 import { Htag } from "../../components";
 import { Widget } from "../../components/Widget/Widget";
+import ChangePassword from "./ChangePassword/ChangePassword";
 import styles from "./EditUser.module.scss";
 import { EditUserProps, TabPanelProps } from "./EditUser.props";
+import PersonalData from "./PersonalData/PersonalData";
+import cn from 'classnames';
+import PersonalBg from "../user/PersonalBg/PersonalBg";
 
 function a11yProps(index: number) {
 	return {
@@ -42,10 +46,12 @@ export default function EditUser(): JSX.Element {
 	};
 
 	return (
+
 		<div className={styles.editUser}>
+			<PersonalBg />
 			<div className={styles.sidebar}>
-				<Widget size='sm' className={styles.w_sidebar}>
-					<Avatar src='https://themesbrand.com/velzon/html/default/assets/images/users/avatar-1.jpg' />
+				<Widget size='sm' className={cn(styles.w_sidebar, styles.headingAvatar)}>
+					<Avatar src='https://themesbrand.com/velzon/html/default/assets/images/users/avatar-1.jpg' className={styles.avatar} />
 					<Htag tag="h4">{`${faker.name.firstName()} ${faker.name.lastName()}`}</Htag>
 					<p>{faker.name.jobType()}</p>
 				</Widget>
@@ -110,106 +116,15 @@ export default function EditUser(): JSX.Element {
 					<Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
 						<Tab label="Personal Details" {...a11yProps(0)} />
 						<Tab label="Change Password" {...a11yProps(1)} />
-						<Tab label="Experience" {...a11yProps(2)} />
+						<Tab label="Transaction History" {...a11yProps(2)} />
 						<Tab label="Privacy Policy" {...a11yProps(3)} />
 					</Tabs>
 				</Box>
 				<TabPanel value={value} index={0}>
-					<div className={styles.tabPanelContainer}>
-						<Box
-							component="form"
-							sx={{
-								'& .MuiTextField-root': { m: 1, width: '25ch' },
-							}}
-							noValidate
-							autoComplete="off"
-						>
-							<div className={styles.col_1}>
-								<label>First Name</label>
-								<OutlinedInput
-									placeholder="First Name"
-									required
-									value="Dava"
-									size="small"
-									fullWidth
-								/>
-							</div>
-							<div className={styles.col_1}>
-								<label>Last Name</label>
-								<OutlinedInput
-									placeholder="Last Name"
-									required
-									value="Dava 9999"
-									size="small"
-									fullWidth
-								/>
-							</div>
-							<div className={styles.col_1}>
-								<label>Phone Number</label>
-								<OutlinedInput
-									placeholder="First Name"
-									required
-									value="+1 (098) 7767645"
-									size="small"
-									fullWidth
-									type="phone"
-								/>
-							</div>
-							<div className={styles.col_1}>
-								<label>Email Address</label>
-								<OutlinedInput
-									size="small"
-									fullWidth
-								/>
-							</div>
-							<div className={styles.col_2}>
-								<label>Joining Date</label>
-								<OutlinedInput
-									size="small"
-									type="date"
-									value='1986-07-22'
-									fullWidth
-								/>
-							</div>
-							<div className={styles.col_1}>
-								<label>Designation</label>
-								<OutlinedInput
-									size="small"
-									fullWidth
-									value='Lead Designer / Developer'
-								/>
-							</div>
-							<div className={styles.col_1}>
-								<label>Website</label>
-								<OutlinedInput
-									size="small"
-									fullWidth
-									value='www.velzon.com'
-								/>
-							</div>
-							<div className={styles.col_2}>
-								<label>Description</label>
-								<TextField
-
-									style={{ width: '100%', margin: '10px 0' }}
-
-									multiline
-									rows={4}
-									hiddenLabel
-									value="Hi I' m Anna Adame,It will be as simple as Occidental; in fact, it will be Occidental.To an English person, it will seem like simplified English, as a skeptical Cambridge friend of mine told me what Occidental is European languages are members of the same family."
-								/>
-							</div>
-							<div className={styles.buttonPannel}>
-								<Button variant="contained">Updates</Button>
-								<Button variant="contained" color="secondary" className={styles.cancelBtn}>Cancel</Button>
-							</div>
-
-						</Box>
-
-					</div>
+					<PersonalData />
 				</TabPanel>
 				<TabPanel value={value} index={1}>
-					Change Password
+					<ChangePassword />
 				</TabPanel>
 				<TabPanel value={value} index={2}>
 					Experience
